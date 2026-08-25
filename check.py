18cb9ad957c248d451dedd3a4f376331c53f539c
eliise1 <129278175+eliise1@users.noreply.github.com>
2026-08-25 15:10:37 +0300

    Update check.py

--- check.py
+++ check.py
@@ -1,8 +1,42 @@
+import os
+import json
 import requests
 
-html = requests.get(
-    "https://linnateater.ee/mangukava/",
-    timeout=30
-).text
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-print(html[15000:30000])
+URL = "https://linnateater.ee/mangukava/"
+
+html = requests.get(URL, timeout=30).text
+
+if os.path.exists("state.json"):
+    with open("state.json", "r", encoding="utf-8") as f:
+        state = json.load(f)
+else:
+    state = {}
+
+current_has_tickets = "Osta pilet" in html
+
+previous_has_tickets = state.get("has_tickets", False)
+
+if current_has_tickets and not previous_has_tickets:
+
+    msg = (
+        "🎭 Linnateatri mängukavas on saadaval pileteid.\n\n"
+        "Kontrolli järgmise 7 päeva etendusi:\n"
+        "https://linnateater.ee/mangukava/"
+    )
+
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": msg
+        },
+        timeout=30
+    )
+
+state["has_tickets"] = current_has_tickets
+
+with open("state.json", "w", encoding="utf-8") as f:
+    json.dump(state, f)
