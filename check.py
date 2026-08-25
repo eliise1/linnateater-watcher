b12a26eb72daa2c1ccb89bc293286bb70fd85d16
eliise1 <129278175+eliise1@users.noreply.github.com>
2026-08-25 14:23:14 +0300

    Update check.py

--- check.py
+++ check.py
@@ -1,15 +1,17 @@
 import os
 import requests
 
+url = "https://linnateater.ee/mangukava/"
+html = requests.get(url).text
+
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-requests.post(
-f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-data={
-"chat_id": CHAT_ID,
-"text": "✅ Linnateatri jälgija töötab!"
-}
-)
-
-print("Test message sent")
+if "Osta pilet" in html:
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": "🎭 Linnateatri mängukavas leidub müügis pileteid:\nhttps://linnateater.ee/mangukava/"
+        }
+    )
