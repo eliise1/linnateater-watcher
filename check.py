ed647e1b50bb065805d7af179dcd6a5490223c96
eliise1 <129278175+eliise1@users.noreply.github.com>
2026-08-25 15:36:16 +0300

    Update check.py

--- check.py
+++ check.py
@@ -1,7 +1,6 @@
 import os
 import json
 import requests
-from datetime import datetime, timedelta
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
@@ -10,18 +9,12 @@ URL = "https://linnateater.ee/mangukava/"
 
 html = requests.get(URL, timeout=30).text
 
-today = datetime.now()
-next_week = today + timedelta(days=7)
-
-# Loeme vana seisu
 if os.path.exists("state.json"):
     with open("state.json", "r", encoding="utf-8") as f:
         state = json.load(f)
 else:
     state = {}
 
-# Lihtne kontroll:
-# kas lehel leidub üldse "Osta pilet"
 has_tickets = "Osta pilet" in html
 
 previous_state = state.get("has_tickets", False)
@@ -33,8 +26,7 @@ if has_tickets and not previous_state:
             "chat_id": CHAT_ID,
             "text": (
                 "🎭 Linnateatri mängukavas leidub müügis pileteid.\n\n"
-                "Kontrolli järgmise 7 päeva etendusi:\n"
-                "https://linnateater.ee/mangukava/"
+                "Vaata: https://linnateater.ee/mangukava/"
             )
         },
         timeout=30
@@ -44,4 +36,3 @@ state["has_tickets"] = has_tickets
 
 with open("state.json", "w", encoding="utf-8") as f:
     json.dump(state, f)
-``
